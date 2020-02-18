0905681bbd8a21b058b3f777427090813358bae2
yoyomanhihi <48712682+yoyomanhihi@users.noreply.github.com>
2020-02-18 19:38:05 +0100

    Beginning of the tuto (Videos 1/2)

--- core/src/com/mygdx/game/ProtectTheKingdom.java
+++ core/src/com/mygdx/game/ProtectTheKingdom.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.Screens.PlayScreen;
 
 public class ProtectTheKingdom extends Game {
 	public SpriteBatch batch;
