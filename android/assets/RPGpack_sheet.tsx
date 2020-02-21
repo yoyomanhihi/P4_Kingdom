0f3e0a32b55ed5b412ea891115c59e62423a5094
yoyomanhihi <48712682+yoyomanhihi@users.noreply.github.com>
2020-02-21 17:28:07 +0100

    Bug partial correction

--- android/assets/RPGpack_sheet.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="RPGpack_sheet" tilewidth="32" tileheight="32" tilecount="1040" columns="40">
- <image source="KenneyRPGpack/Spritesheet/RPGpack_sheet.png" width="1280" height="832"/>
- <tile id="411">
-  <properties>
-   <property name="Meilleur" value=""/>
-  </properties>
- </tile>
-</tileset>
