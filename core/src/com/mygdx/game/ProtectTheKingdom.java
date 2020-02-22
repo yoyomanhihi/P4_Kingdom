06a30d15c3967de14f78b620459f0b0b79908641
Co-Thn <35226611+coren99@users.noreply.github.com>
2020-02-22 16:01:02 +0100

    Add menu

--- core/src/com/mygdx/game/ProtectTheKingdom.java
+++ core/src/com/mygdx/game/ProtectTheKingdom.java
@@ -6,16 +6,23 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.Screens.MenuScreen;
 import com.mygdx.game.Screens.PlayScreen;
 
 public class ProtectTheKingdom extends Game {
 	public SpriteBatch batch;
 	//Texture img;
+	private MenuScreen menuScreen;
+	private PlayScreen playScreen;
+
+	public final static int MENU = 0;
+	public final static int PLAY = 1;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		setScreen(new PlayScreen(this));
+		menuScreen = new MenuScreen(this);
+		setScreen(menuScreen);
 		//img = new Texture("badlogic.jpg");
 	}
 
@@ -27,5 +34,18 @@ public class ProtectTheKingdom extends Game {
 	@Override
 	public void dispose () {
 		batch.dispose();
+	}
+
+	public void changeScreen(int screen){
+		switch (screen){
+			case MENU:
+				if (menuScreen == null) menuScreen = new MenuScreen(this);
+				this.setScreen(menuScreen);
+				break;
+			case PLAY:
+				if(playScreen == null) playScreen = new PlayScreen(this);
+				this.setScreen(playScreen);
+				break;
+		}
 	}
 }
---
+++ core/src/com/mygdx/game/Screens/MenuScreen.java
@@ -0,0 +1,90 @@
+package com.mygdx.game.Screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.mygdx.game.ProtectTheKingdom;
+
+public class MenuScreen implements Screen {
+
+    private final Stage stage;
+    private ProtectTheKingdom game;
+
+    public MenuScreen(ProtectTheKingdom game){
+        this.game = game;
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void show() {
+        Table table = new Table();
+        table.setFillParent(true);
+        table.setDebug(true);
+        stage.addActor(table);
+
+        Skin skin = new Skin(Gdx.files.internal("skin/sgx-ui.json"));
+
+        TextButton newGame = new TextButton("New Game", skin);
+        TextButton exit = new TextButton("Exit", skin);
+
+        table.add(newGame).fillX().uniformX();
+        table.row().pad(10, 0, 10, 0);
+        table.add(exit).fillX().uniformX();
+
+        exit.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Gdx.app.exit();
+            }
+        });
+
+        newGame.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                game.changeScreen(ProtectTheKingdom.PLAY);
+            }
+        });
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(),1/30f));
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+}
